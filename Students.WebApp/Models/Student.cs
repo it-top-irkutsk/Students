57d0047502d64860ed287d3a0135f1ded0b112d9
Starinin Andrey <starininandrey@gmail.com>
2022-03-10 16:15:34 +0300

    Init

---
+++ Students.WebApp/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Students.WebApp.Models;
+
+public record Student
+{
+    [JsonPropertyName("id")]
+    public int Id { get; init; }
+    
+    [JsonPropertyName("first_name")]
+    public string FirstName { get; init; }
+    
+    [JsonPropertyName("last_name")]
+    public string LastName { get; init; }
+    
+    [JsonPropertyName("faculty")]
+    public string Faculty { get; init; }
+}
