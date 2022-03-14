7b15067408a416bc8a018f8ec4cf83bcb9db5c19
Starinin Andrey <starininandrey@gmail.com>
2022-03-14 14:13:38 +0300

    Init

---
+++ Students.WebApp/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Students.WebApp.Models;
+
+namespace Students.WebApp.Controllers;
+
+public class StudentController : Controller
+{
+    private Models.Students _students;
+
+
+    public StudentController()
+    {
+        _students = Models.Students.ImportFromJson("./wwwroot/students.json").Result;
+    }
+
+    public IActionResult Index()
+    {
+        return View("Index", _students);
+    }
+
+    public IActionResult Student(string id)
+    {
+        var student = int.TryParse(id, out var Id) ? _students.GetStudentById(Id) : new Student();
+        
+        return View("Student", student);
+    }
+}
---
+++ Students.WebApp/Models/Students.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace Students.WebApp.Models;
+
+public class Students
+{
+    private readonly List<Student> _students;
+
+    public Students()
+    {
+        _students = new List<Student>();
+    }
+    public Students(List<Student> students)
+    {
+        _students = students;
+    }
+
+    public IEnumerable<Student> GetAllStudents() => _students;
+
+    public Student GetStudentById(int id) => _students.Find(s => s.Id == id) ?? throw new InvalidOperationException();
+
+    public static async Task<Students> ImportFromJson(string path)
+    {
+        await using var file = new FileStream(path, FileMode.Open);
+        var students = await JsonSerializer.DeserializeAsync<List<Student>>(file);
+        return students is not null ? new Students(students) : new Students();
+    }
+}
